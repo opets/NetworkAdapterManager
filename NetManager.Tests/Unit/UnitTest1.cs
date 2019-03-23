c9107eb193bb96881e5043528436505272021516
OleksandrPets <pec.alex@gmail.com>
2019-03-23 17:33:07 +0200

    1

--- NetManager.Tests/Unit/UnitTest1.cs
+++ NetManager.Tests/Unit/UnitTest1.cs
@@ -1,19 +1,14 @@
 using NetManager.Domain;
-using NUnit.Framework;
+using Xunit;
 
 namespace NetManager.Tests.Unit {
 
-	public class Tests {
-		[SetUp]
-		public void Setup() {
-		}
-
-		[Test]
-		public void When_Test_Pass() {
+	public class UnitTest1 {
+		[Fact]
+		public void Test1() {
 			var c = new Class1();
-			Assert.AreEqual("123456",c.TestMethod());
+			Assert.Equal( "123456", c.TestMethod() );
 		}
-
-
 	}
+
 }
